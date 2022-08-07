e65dff66598bc6ae6211220b893057706d5dc42a
Dima <dkozyarskij@gmail.com>
2022-08-07 23:03:53 +0500

    cleaned'n improved

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import UserStore from './store/UserStore';
 import reportWebVitals from './reportWebVitals';
+import DeviceStore from './store/DeviceStore';
+
+export const Context = createContext(null)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Context.Provider value={{
+      user: new UserStore(),
+      device: new DeviceStore()
+    }}>
+      <App />
+    </Context.Provider>
   </React.StrictMode>
 );
 
